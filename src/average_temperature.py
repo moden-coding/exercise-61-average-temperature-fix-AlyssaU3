7afc72c972a134d4abf306aa6ed227b6f3c9d101
AlyssaU3 <alys4321c@gmail.com>
2025-10-28 13:27:58 -0400

    61 done

--- src/average_temperature.py
+++ src/average_temperature.py
@@ -3,11 +3,17 @@
 import pandas as pd
 
 def average_temperature():
-    pass
-
+    temp=pd.read_csv("src/kumpula-weather-2017.csv")
+    # print(temp.head())
+    # print(temp.columns)
+    july= temp["m"]==7
+    temp2= temp[july]["Air temperature (degC)"].mean()
+    return temp2
 def main():
+    
     result = average_temperature()
-    print(f"Average temperature in July: {result:.1f}")
-
+    print(f"Average temperature in July: {result}")
+    # print(f"Average temperature in July: {result:.1f}")
+    
 if __name__ == "__main__":
     main()
